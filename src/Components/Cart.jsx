b310fae0e417402649258e59b804f1abbef0d3a5
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-08-18 00:02:42 +0530

    some commits

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { remove } from '../store/createSlice';
+import {Toaster,toast} from 'react-hot-toast'
 import './Cart.css'
 
 const Cart = () => {
     const products = useSelector((state)=>state.cart);
-    const dispatch = useDispatch;
+    const dispatch = useDispatch();
+  
+
+
     
     const handleRemove=(product)=>{
 dispatch(remove(product));
+toast.error("removed from cart");
     }
   return (
     <div>
@@ -24,7 +29,7 @@ dispatch(remove(product));
                         <div className="price">{main.price}</div>
                         <button className="remove" onClick={()=>handleRemove(main.id)}>remove</button>
                     </div>
-                    
+                    <Toaster/>
                                     </div>
                 )
               
